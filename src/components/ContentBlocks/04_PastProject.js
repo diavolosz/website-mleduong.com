e54a19349bde5291802d4f90261cf698a5718e54
diavolos <michaeldiavolos@gmail.com>
2024-10-04 18:49:45 -0400

    minor project update

--- src/components/ContentBlocks/04_PastProject.js
+++ src/components/ContentBlocks/04_PastProject.js
@@ -27,7 +27,7 @@ export default function PastProjectbBlock() {
     <div className={`article-block-4-container ${theme}`}>
       <p className='section-title text'>MY WORKS</p>
 
-      <div className="inner-container s1 border background-reverse">
+      {/* <div className="inner-container s1 border background-reverse">
         <div className={`image-card-left ${P1}`}>
           <img className="gif-showcase" src='img/gifs/MLD.gif' alt='testing' onClick={handleHoverLeftP1} />
         </div>
@@ -42,10 +42,45 @@ export default function PastProjectbBlock() {
             </div>
           </div>
         </div>
+      </div> */}
+
+      <div className="inner-container s1 border background-reverse">
+        <div className={`image-card-left ${P1}`}>
+          <img className="gif-showcase" src='img/gifs/VOI.gif' alt='testing' onClick={handleHoverLeftP1} />
+        </div>
+        <div className="description-card-right background-reverse">
+          <div className='project-description-container'>
+            <div className='project-title text-reverse'> Voithos Labs</div>
+            <div className='project-description text-reverse'>
+              Landing page for Voithos Labs that offers cutting-edge technology solutions, specializing in innovative software and digital services to boost productivity.
+            </div>
+            <div className='button-wrapper background text'>
+              <a href='https://www.voithoslabs.com/' className='button-to-project' target="_blank">Link</a>
+            </div>
+          </div>
+        </div>
       </div>
 
 
       <div className="inner-container s2 border background-reverse">
+        <div className="description-card-left background-reverse">
+          <div className='project-description-container'>
+            <div className='project-title text-reverse'> Vnote </div>
+            <div className='project-description text-reverse'>
+              a sleek, user-friendly note-taking platform designed for effortless organization, perfect for individuals and teams looking to keep their ideas and tasks in one place.
+            </div>
+            <div className='button-wrapper background text'>
+              <a href='https://notes.voithos.app/' className='button-to-project' target="_blank">Link</a>
+            </div>
+          </div>
+        </div>
+        <div className={`image-card-right ${P2}`}>
+          <img className="gif-showcase" src='img/gifs/VNO.gif' alt='testing' onClick={handleHoverRightP2} />
+        </div>
+      </div>
+
+
+      {/* <div className="inner-container s2 border background-reverse">
         <div className="description-card-left background-reverse">
           <div className='project-description-container'>
             <div className='project-title text-reverse'> BayStreetbets </div>
@@ -60,21 +95,21 @@ export default function PastProjectbBlock() {
         <div className={`image-card-right ${P2}`}>
           <img className="gif-showcase" src='img/gifs/BBS.gif' alt='testing' onClick={handleHoverRightP2} />
         </div>
-      </div>
+      </div> */}
 
 
       <div className="inner-container s3 border background-reverse">
         <div className={`image-card-left ${P3}`}>
-          <img className="gif-showcase" src='img/gifs/PNT.gif' alt='testing' onClick={handleHoverLeftP3} />
+          <img className="gif-showcase" src='img/gifs/BBS.gif' alt='testing' onClick={handleHoverLeftP3} />
         </div>
         <div className="description-card-right background-reverse">
           <div className='project-description-container'>
-            <div className='project-title text-reverse'> Pinnet </div>
+            <div className='project-title text-reverse'> BayStreetbets </div>
             <div className='project-description text-reverse'>
-              An Address Notebook. Pinnet offers users the ability to pin and save their favorite place to eat or hangout by creating their own sharable address list.
+              A Stock Trading Platform that allow user to keep track of their stocks and portfolios. Has unique feature for trade competitions with peers to crown the winner.
             </div>
             <div className='button-wrapper background text'>
-              <a href='https://github.com/diavolosz/Lighthouse-MT-wikiMaps' className='button-to-project' target="_blank">GitHub</a>
+              <a href='https://github.com/diavolosz/BayStreetBets' className='button-to-project' target="_blank">GitHub</a>
             </div>
           </div>
         </div>
